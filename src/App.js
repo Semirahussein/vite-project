3139d8dcb9f0ebe10f6819e8c5bbbc30b7cf5e67
semira hussein <semirahussein71@gmail.com>
2023-12-20 00:04:23 +0300

    the todo list

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Comp from './comp';
+
+function App(){
+    return(
+<div className='App'>
+<h1>hello world</h1>
+<comp/> 
+
+</div>
+    );
+}
+export default App;
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import groupList from "./groupList/groupList";
-function App() {
-  return (
-  <div><groupList></groupList></div>
+// import groupList from "./groupList/groupList";
+// function App() {
+//   return (
+//   <div><groupList></groupList></div>
   
-  )
+//   )
+// }
+
+// export default App;
+
+
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} onRemove={removeTodo} onToggle={toggleTodo} />
+      <AddTodoForm onAdd={addTodo} />
+    </div>
+  );
 }
 
 export default App;
